886c31f9a40f4298a1c019b20c0d4a7d75c7a139
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-02-20 11:13:09 -0500

    Completes Conditionals Extra Skills

---
+++ conditionals2.rb
@@ -0,0 +1,100 @@
+# 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# variable = 10
+# if variable == 10
+#   puts "0 because the number is equal to 10"
+# else
+#   puts "-1 because the number is not 10"
+# end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+# variable = 12
+# if variable < 10
+#   puts "-1"
+# elsif variable > 10
+#   puts "1"
+# elsif variable == 10
+#   puts "0"
+# end
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# variable1 = 9
+# variable2 = 7
+# if variable1 < 10 && variable2 < 10
+#   puts "Both less than 10, #1"
+# else
+#   puts "Not both less than 10, #0"
+# end
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+# variable = 0
+# if variable > 9000
+#   puts "Over 9000 prints 1"
+# else
+#   puts "Not over 9,000 prints -1"
+# end
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+# variable = 29
+# if variable < 10
+#   puts "#{variable} is less than 10, prints 9"
+# elsif variable < 20
+#   puts "19 because #{variable} is less than 20"
+# elsif variable < 30
+#   puts "29 because #{variable} is less than 30."
+# else
+#   puts "-1"
+# end
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+# variable1 = 2
+# variable2 = 3
+# if variable1 > 10 || variable2 > 10
+#   puts "One of the numbers is greater than 10, puts 100"
+# else
+#   puts "-100"
+# end
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+# variable = 3
+# if variable < 0
+#   puts "1776"
+# else
+#   puts "1979"
+# end
+
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+# variable = 3
+# if variable == 100
+#   puts "100"
+# elsif variable == 99
+#   puts "99"
+# else
+#   puts "0"
+# end
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+# variable1 = 3
+# variable2 = 3
+# if variable1 < 0 && variable2 > 0
+#   puts "1"
+# else
+#   puts "0"
+# end
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only 8 print statement should occur).
+# variable = 5
+# if variable > 80
+#   puts "5"
+# elsif variable > 60
+#   puts "4"
+# elsif variable > 40
+#   puts "3"
+# elsif variable > 20
+#   puts "2"
+# else
+#   puts "1"
+# end
+
+# SOLUTIONS: https://gist.github.com/peterxjang/aee70f966f0f725609eee89b06e8a6c0
