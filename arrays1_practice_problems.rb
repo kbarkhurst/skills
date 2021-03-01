657bc0670f9b3f42552f26b8b3c69f9289d10942
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-01 08:53:14 -0500

    Practice Arrays

---
+++ arrays1_practice_problems.rb
@@ -0,0 +1,76 @@
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+# array = ["word1", "word2", "word3"]
+# array << "word4"
+# array << "word5"
+# p array
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+# letter = ["a", "b", "c", "d"]
+# letter[1] = 3
+# p letter
+
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+# array = [20, 30, 40, 50, 60]
+# i = 0
+# while i < array.length
+#   puts array[i]
+#   i += 1
+# end
+
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+# array = [1]
+# array << 2
+# array << 3
+# array << 4
+# p array
+
+
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+# strings = ["this", "is", "a"]
+# strings[2] = strings[2].upcase
+# p strings
+
+
+
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+# names = ["Kelly", "Kim", "Kara"]
+# i = 0
+# while i < names.length
+#   puts names[i]
+#   i += 1
+# end
+# or from the array just 
+# puts names
+
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+# strings = ["string1", "string2"]
+# strings << "string3"
+# p strings
+
+
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+# numbers = [100, 500, 300, 200, 0]
+# numbers[0] = numbers[0]*10
+# p numbers
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+# numbers = [3000, -400]
+# i = 0
+# while i < numbers.length
+#   puts numbers[i]
+#   i += 1
+# end
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+# countries = ["Mexico", "Aruba", "Spain"]
+# countries << "United States"
+# p countries
+
+# SOLUTIONS: https://gist.github.com/peterxjang/7095a2b19e1da2cc18d4a0dcd66cb8f1
