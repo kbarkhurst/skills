92520a51dc2176bc2fec4a39e7eb57167ee618c8
Kelly Barkhurst <kellybarkhurst@gmail.com>
2021-03-01 08:28:37 -0500

    Skills Drills

---
+++ loops2_practice_problems.rb
@@ -0,0 +1,78 @@
+# 1. Write a while loop to print the numbers 1 through 10.
+# index = 1
+# while index < 11
+#   puts index
+#   index = index + 1
+# end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+# 5.times do
+#   puts "hello"
+# end
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+# while true
+#   puts "Enter a word. When you want to quit, type stop. "
+#   input = gets.chomp.downcase
+#   if input == "stop"
+#     break
+#   end
+# end
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+# index = 0
+# while index <= 100
+#   puts index
+#   index = index + 5
+# end
+
+# 5. Write a while loop that prints the number 9000 ten times.
+# index = 10
+# while index <= 10 && index > 0
+#   puts 9000
+#   index = index - 1
+# end
+
+# count = 0
+# while count < 10
+#   puts 9000
+#   count = count + 1
+# end
+
+# 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+# while true
+#   puts "enter a number"
+#   number = gets.chomp.to_i
+#   if number > 10
+#     break
+#   end
+# end
+
+
+# 7. Write a while loop that prints the numbers 50 to 70.
+# count = 50
+# while count <= 70
+#   puts count
+#   count = count + 1
+# end
+
+# 8. Write a while loop that prints the phrase "Around the world" 144 times.
+# count = 1
+# while count <= 144
+#   puts "#{count}: Around the word"
+#   count = count + 1
+# end
+# 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+# while true
+#   puts "enter a word"
+#   length = gets.chomp.length
+#   if length > 5
+#     break
+#   end
+# end
+
+# 10. Write a while loop that prints the even numbers from 2 to 40.
+# number = 2
+# while number <= 40
+#   puts number
+#   number = number + 2
+# end
